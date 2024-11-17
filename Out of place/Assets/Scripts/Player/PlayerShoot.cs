ba96719897a5279f3676e53ccdb5bdf6058d60d6
Ame (Adjamé T.R) <adjame.tel.roz@gmail.com>
2024-11-17 10:56:13 +0100

    deplacement des sons

--- Out of place/Assets/Scripts/Player/PlayerShoot.cs
+++ Out of place/Assets/Scripts/Player/PlayerShoot.cs
@@ -10,13 +10,18 @@ public class PlayerShoot : MonoBehaviour
 
     [SerializeField] private Camera fpsCam;
     [SerializeField] private float aimDistanceFromCamera = 1000f;
-    [SerializeField] private PauseManager pauseManager; 
+    [SerializeField] private PauseManager pauseManager;
 
+    [Header("Sound Effects")]
+    [SerializeField] private AudioClip hitAlienSound;
+    [SerializeField] private AudioClip missSound;
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         playerManager = GameObject.Find("Player").GetComponent<PlayerManager>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -35,6 +40,7 @@ public class PlayerShoot : MonoBehaviour
 
     void Aim()
     {
+        
         float screenX = Screen.width / 2;
         float screenY = Screen.height / 2;
 
@@ -51,7 +57,8 @@ public class PlayerShoot : MonoBehaviour
 
         if (hit.transform.CompareTag("Mimic"))
         {
-            for(int i = 0; i < lightDoor.Length; i++)
+            PlaySound(hitAlienSound);
+            for (int i = 0; i < lightDoor.Length; i++)
             {
                 lightDoor[i].SetLightOn(hit.transform.GetComponent<MimicManager>().GetIdMimic());
             }
@@ -61,11 +68,19 @@ public class PlayerShoot : MonoBehaviour
         }
         else
         {
+            PlaySound(missSound);
             GameObject impact = Instantiate(bulletPrefab, hit.point, Quaternion.LookRotation(hit.normal));
             Debug.Log(impact.transform.rotation);
             impact.transform.position = new Vector3(impact.transform.position.x, impact.transform.position.y + 0.001f, impact.transform.position.z);
             playerManager.numberOfHP -= 1;
             Debug.Log("Vie restante : " + playerManager.numberOfHP);
+        }
+    }
+    void PlaySound(AudioClip clip)
+    {
+        if (audioSource != null && clip != null)
+        {
+            audioSource.PlayOneShot(clip);
         }
     }
 }
